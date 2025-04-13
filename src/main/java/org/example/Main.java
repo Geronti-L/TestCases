de83162933c226a76400942ab4b52c7a09738c44
Liparteliani <gerontiliparteliani@gmail.com>
2025-04-13 16:35:44 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int i=0;
-        if(i<=10){
+        while(i<=10){
             System.out.println("hi "+i);
             i++;
         }
