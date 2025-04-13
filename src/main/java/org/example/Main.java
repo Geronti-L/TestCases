790b55a779148c27f1fbf86b0031e0e77f44285e
Liparteliani <gerontiliparteliani@gmail.com>
2025-04-13 16:32:23 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int i=0;
+        if(i<=10){
+            System.out.println("hi "+i);
+            i++;
+        }
     }
 }
